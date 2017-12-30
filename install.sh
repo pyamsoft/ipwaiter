ffd1dddc37bf0abdaae00c7824364a54b13fc88b
pyamsoft <pyam.soft@gmail.com>
2017-12-30 13:56:06 -0800

    Pass DESTDIR to the install script

--- install.sh
+++ install.sh
@@ -2,37 +2,41 @@
 
 installer()
 {
+  printf -- 'Installing...\n.'
+
   # Prepare directories
-  mkdir -p /usr/bin
-  mkdir -p /etc/ipwaiter/orders
-  mkdir -p /usr/lib/systemd/system
+  mkdir -p "${DESTDIR}/usr/bin" || return 1
+  mkdir -p "${DESTDIR}/etc/ipwaiter/orders" || return 1
+  mkdir -p "${DESTDIR}/usr/lib/systemd/system" || return 1
 
   # Install script
-  install -m 755 -D ./ipwaiter /usr/bin || return 1
+  install -m 755 -D ipwaiter "${DESTDIR}/usr/bin" || return 1
 
   # Install system config
-  install -m 644 -D ./conf/system.conf /etc/ipwaiter || return 1
+  install -m 644 -D conf/system.conf "${DESTDIR}/etc/ipwaiter" || return 1
 
   # Install orders
-  install -m 644 -D ./conf/orders/*.order /etc/ipwaiter/orders || return 1
+  install -m 644 -D conf/orders/*.order "${DESTDIR}/etc/ipwaiter/orders" || return 1
 
   # Install systemd service
-  install -m 644 -D ./conf/systemd/ipwaiter.service /usr/lib/systemd/system || return 1
+  install -m 644 -D conf/systemd/ipwaiter.service "${DESTDIR}/usr/lib/systemd/system" || return 1
 }
 
 uninstaller()
 {
+  printf -- 'Uninstalling...\n.'
+
   # Remove script
-  rm -f /usr/bin/ipwaiter
+  rm -f "${DESTDIR}/usr/bin/ipwaiter" || return 1
 
   # Remove configuration directory
-  rm -r -f /etc/ipwaiter
+  rm -r -f "${DESTDIR}/etc/ipwaiter" || return 1
 
   # Stop the service
-  systemctl disable ipwaiter.service
+  systemctl disable ipwaiter.service || return 1
 
   # Remove the service
-  rm -f /usr/lib/systemd/system/ipwaiter.service
+  rm -f "${DESTDIR}/usr/lib/systemd/system/ipwaiter.service" || return 1
 }
 
 if [ "$(id -u)" -ne 0 ]; then
