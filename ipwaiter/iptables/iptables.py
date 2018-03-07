ab9cff45a3da20a42e66f172ba1ab1df8293b1e9
pyamsoft <pyam.soft@gmail.com>
2018-03-06 21:38:16 -0800

    Add iptables abstraction file

---
+++ ipwaiter/iptables/iptables.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import sh
+
+from ..logger.logger import Logger
+
+
+class Iptables:
+
+    def __init__(self):
+        self._cmd = sh.Command("iptables")
+
+    def add(self, table, chain, *args):
+        if not table or not chain or not args:
+            Logger.fatal("""Failed add() to iptables, missing argument
+table: {}, chain: {}, args: {}""".format(table, chain, args))
+        else:
+            command = ["-t", table, "-A", chain]
+            command += args
+            result = self._cmd(command)
+            return result.exit_code == 0
+
+    def link(self, table, parent_chain, target_chain):
+        if not table or not parent_chain or not target_chain:
+            Logger.fatal("""Failed link() to iptables, missing argument
+table: {}, parent_chain: {}, target_chain: {}"""
+                         .format(table, parent_chain, target_chain))
+        else:
+            result = self._cmd(
+                "-t", table,
+                "-A", parent_chain,
+                "-j", target_chain
+            )
+            return result.exit_code == 0
+
+    def create(self, table, chain):
+        if not table or not chain:
+            Logger.fatal("""Failed create() iptables, missing argument
+table: {}, chain: {}""".format(table, chain))
+        else:
+            result = self._cmd("-t", table, "-N", chain)
+            return result.exit_code == 0
+
+    def flush(self, table, chain):
+        if not table or not chain:
+            Logger.fatal("""Failed flush() iptables, missing argument
+table: {}, chain: {}""".format(table, chain))
+        else:
+            result = self._cmd("-t", table, "-F", chain)
+            return result.exit_code == 0
+
+    def unlink(self, table, parent_chain, target_chain):
+        if not table or not parent_chain or not target_chain:
+            Logger.fatal("""Failed unlink() from iptables, missing argument
+table: {}, parent_chain: {}, target_chain: {}"""
+                         .format(table, parent_chain, target_chain))
+        else:
+            result = self._cmd(
+                "-t", table,
+                "-D", parent_chain,
+                "-j", target_chain
+            )
+            return result.exit_code == 0
+
+    def delete(self, table, chain):
+        if not table or not chain:
+            Logger.fatal("""Failed delete() from iptables, missing argument
+table: {}, chain: {}""".format(table, chain))
+        else:
+            result = self._cmd("-t", table, "-X", chain)
+            return result.exit_code == 0
+
+    def exists(self, table, chain):
+        if not table or not chain:
+            Logger.fatal("""Failed exists() in iptables, missing argument
+table: {}, chain: {}""".format(table, chain))
+        else:
+            result = self._cmd("-t", table, "-N", chain)
+            return result.exit_code == 0
