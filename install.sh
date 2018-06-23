add7ccf7a221d426b6b225cc4d44513b68eaa7a1
pyamsoft <pyam.soft@gmail.com>
2018-06-22 17:44:58 -0700

    Add bash completion and clean install

--- install.sh
+++ install.sh
@@ -6,12 +6,19 @@ installer()
 {
   printf -- 'Installing...\n'
 
+  # Set umask to permissive
+  umask 022
+
   # Prepare directories
+  mkdir -p "${DESTDIR}/etc/ipwaiter/install" || return 1
   mkdir -p "${DESTDIR}/etc/ipwaiter/orders" || return 1
   mkdir -p "${DESTDIR}/usr/lib/systemd/system" || return 1
 
   # Install using python
-  python setup.py install --root="${DESTDIR}" --optimize=1 || return 1
+  python setup.py install \
+    --record="${DESTDIR}/etc/ipwaiter/install/files.txt" \
+    --root="${DESTDIR}" \
+    --optimize=1 || return 1
 
   # Install system config
   install -m 644 -D conf/system.conf "${DESTDIR}/etc/ipwaiter" || return 1
@@ -21,19 +28,49 @@ installer()
 
   # Install systemd service
   install -m 644 -D conf/systemd/ipwaiter.service "${DESTDIR}/usr/lib/systemd/system" || return 1
+
+  # Install license
+  install -m 644 -D LICENSE "${DESTDIR}/usr/share/licenses/ipwaiter" || return 1
+
+  # Install bash completion
+  install -m 644 -D res/shell/bash/bash_completion "${DESTDIR}/usr/share/bash-completion/completions/ipwaiter" || return 1
 }
 
 uninstaller()
 {
   printf -- 'Uninstalling...\n'
 
-  pip uninstall ipwaiter || return 1
+  # Set umask to permissive
+  umask 022
+
+  # If pip knows about the package, it can uninstall it.
+  if pip show ipwaiter > /dev/null; then
+    pip uninstall -y ipwaiter || return 1
+  else
+    printf -- 'pip cannot find ipwaiter, installed to a non standard location?\n'
+    install_history="${DESTDIR}/etc/ipwaiter/install/files.txt"
+    if [ -e "${install_history}" ] && [ -r "${install_history}" ]; then
+      # We can uninstall files manually
+      while read -r file; do
+        rm -f "${DESTDIR}/${file}" || return 1
+      done < "${install_history}"
+    else
+      printf -- 'Cannot find list of installed files?\n'
+      return 1
+    fi
+  fi
 
   # Remove configuration directory
   rm -r -f "${DESTDIR}/etc/ipwaiter" || return 1
 
   # Remove the service
   rm -f "${DESTDIR}/usr/lib/systemd/system/ipwaiter.service" || return 1
+
+  # Remove license and directory
+  rm -r -f "${DESTDIR}/usr/share/licenses/ipwaiter" || return 1
+
+  # Remove bash completion
+  rm -f "${DESTDIR}/usr/share/bash-completion/completions/ipwaiter" || return 1
 }
 
 if [ "$(id -u)" -ne 0 ]; then
@@ -46,10 +83,17 @@ if [ ! -e ./ipwaiter ]; then
   exit 1
 fi
 
-if [ "$1" = "uninstall" ]; then
+if [ "$1" = "install" ]; then
+  installer || exit 1
+elif [ "$1" = "uninstall" ]; then
   uninstaller || exit 1
 else
-  installer || exit 1
+  printf -- '%s\n' "$(cat << EOF
+  commands:
+    install
+    uninstall
+EOF
+)"
 fi
 
 exit 0
