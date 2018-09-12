ad08e264274423c1e29eddeaf2b53e8470db1040
pyamsoft <pyam.soft@gmail.com>
2018-09-11 18:20:36 -0700

    Fire and hire just pass all orders to their functions instead of looping themselves

--- ipwaiter/orders/waiter.py
+++ ipwaiter/orders/waiter.py
@@ -43,7 +43,7 @@ class Waiter:
         return name, table, chain, parent, order
 
     def add_order(self, order, raw, opts):
-        self._add_order(order, raw, opts, True)
+        self._add_order(order, raw, opts, report=True)
 
     def _add_order(self, order, raw, opts, report):
         if not order:
@@ -53,6 +53,7 @@ class Waiter:
         o_names = order[1:]
 
         for o_name in o_names:
+            o_name = o_name.strip()
             name, table, chain, parent, path = self._verify(o_name, raw, o_chain)
             self._place_order(name, table, chain, parent, path, raw, opts, report)
 
@@ -90,7 +91,7 @@ class Waiter:
             Logger.log(f"ipwaiter has placed order: {name}")
 
     def delete_order(self, order, raw):
-        self._delete_order(order, raw, True)
+        self._delete_order(order, raw, report=True)
 
     def _delete_order(self, order, raw, report):
         if not order:
@@ -100,6 +101,7 @@ class Waiter:
         o_names = order[1:]
 
         for o_name in o_names:
+            o_name = o_name.strip()
             name, table, chain, parent, path = self._verify(o_name, raw, o_chain)
             self._remove_order(name, table, chain, parent, report)
 
@@ -140,40 +142,43 @@ class Waiter:
     def hire_waiter(self, opts):
         Logger.log("Hiring new ipwaiter")
         order_dict = self._system_conf.parse()
-        for order in order_dict["FILTER_INPUT"]:
-            order = order.strip()
-            if order:
-                self._add_order(("input", order), False, opts, False)
-        for order in order_dict["FILTER_FORWARD"]:
-            order = order.strip()
-            if order:
-                self._add_order(("forward", order), False, opts, False)
-        for order in order_dict["FILTER_OUTPUT"]:
-            order = order.strip()
-            if order:
-                self._add_order(("output", order), False, opts, False)
-        for order in order_dict["RAW_OUTPUT"]:
-            order = order.strip()
-            if order:
-                self._add_order(("output", order), True, opts, False)
+
+        orders = order_dict["FILTER_INPUT"]
+        if orders:
+            self._add_order(("input", orders), raw=False, opts=opts, report=False)
+
+        orders = order_dict["FILTER_FORWARD"]
+        if orders:
+            self._add_order(("forward", orders), raw=False, opts=opts, report=False)
+
+        orders = order_dict["FILTER_OUTPUT"]
+        if orders:
+            self._add_order(("output", orders), raw=False, opts=opts, report=False)
+
+        orders = order_dict["RAW_OUTPUT"]
+        if orders:
+            self._add_order(("output", orders), raw=True, opts=opts, report=False)
+
         Logger.log("Hired ipwaiter")
 
     def fire_waiter(self):
         Logger.log("Firing old ipwaiter")
 
+        orders = []
         for order in os.listdir(self._order_dir):
             abspath = utils.to_absolute_path(self._order_dir, order)
             if os.path.isfile(abspath) and abspath.endswith(".order"):
                 base_order = os.path.basename(abspath)
                 base_order = re.sub("\.order$", "", base_order)
+                orders.append(base_order)
 
-                # Delete all not raw
-                self._delete_order(("input", base_order), False, False)
-                self._delete_order(("forward", base_order), False, False)
-                self._delete_order(("output", base_order), False, False)
+        # Delete all not raw
+        self._delete_order(("input", orders), raw=False, report=False)
+        self._delete_order(("forward", orders), raw=False, report=False)
+        self._delete_order(("output", orders), raw=False, report=False)
 
-                # Delete all raw
-                self._delete_order(("output", base_order), True, False)
+        # Delete all raw
+        self._delete_order(("output", orders), raw=True, report=False)
 
         # Delete the order chains
         self._iptables.flush("filter", "input_orders")
